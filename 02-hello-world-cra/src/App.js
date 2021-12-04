3f7583f92113041477880beaf375f993bf9e96b0
JB <jluisbarrios787@gmail.com>
2021-12-03 20:03:10 -0500

    primeros pasos react

---
+++ 02-hello-world-cra/src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import {generate as id} from 'shortid' //clase ARRAYS libreria shortid
+
+// SINTAXIS JSX Y CONDICIONALES
+// function App() {
+//   const name = "Jorge"
+//   const age = 11
+//   const login = false
+
+//   // if(login)
+//   //   return (    
+//   //     <>
+//   //       <h1>Hello World</h1>
+//   //       <h1>Hello World 2</h1>
+//   //       <img src="" alt=""/>
+//   //       <p>Hola mi nombre es {name} y tengo {age *2}</p>
+//   //     </>
+//   //   );
+//   //   else
+//   //     return <p>Usuario anónimo</p>
+
+//   // Operador ternario
+//   // return login ? <p>Hola mi nombre es {name} y tengo {age *2}</p> : <p>Usuario anónimo</p>
+
+//   // Operador ternario simplificado &&
+//   return login && <p>Hola mi nombre es {name} y tengo {age *2}</p>
+//   }
+
+//  ARRAYS  para los id es mejor instalar una librerian desde npm llamada shortid => npm i shortid
+function App(){
+  const numbers = [1, 2, 3, 4, 5]
+  const names = ['Dorian', 'Laura', 'Alex']
+
+  // return <h1>{numbers}</h1>
+  return(
+    // <ul>
+    //   <li>{names}</li>
+    // </ul>
+    <ul>
+      {
+        names.map(name=>{
+          return <li key={id()}>{name}  {id()}</li>
+        })
+      }
+    </ul>
+  )
+}
+
+export default App;
