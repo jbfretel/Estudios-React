89d555b0958d93e771291e17f8d9c703642319e2
JB <jluisbarrios787@gmail.com>
2021-12-04 20:18:18 -0500

    instalación de apheth sort para ordenamiento alfabetico de props en componentes

--- 03-components/src/App.js
+++ 03-components/src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <>
       <Header/>
-      <Person name="Carlos" age={28}/>
-      <Person name="Martha" age={50}/>
-      <Person name="Emilia" age={45}/>
+      <Person name="Carlos" age={28} birthday={new Date(1994,5,4)}/>
+      <Person name="Martha" age={31} birthday={new Date(1990,4,25)}/>
+      <Person name="Emilia" age={53} birthday={new Date(1968,7,14)}/>
     </>
     
   );
--- 03-components/src/components/Person.jsx
+++ 03-components/src/components/Person.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Person = ({name,age}) => { //destructuracion de props forma actual de trabajar con react
+const Person = ({name,age,birthday}) => { //destructuracion de props forma actual de trabajar con react
   return (
     
-    <p>Soy {name} y tengo {age} años</p>
+    <p>Soy {name} y tengo {age} años, naci en {birthday.getFullYear()}</p>
   )
 
 }
